c68dbec5d0e9ae262b531c363d20d112fcd9101e
JecoLantern <jbayonke@hotmail.com>
2018-10-20 09:38:01 -0700

    frontpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+
+    function startScreen() {
+        startButton = "<div class='row'><div class='col offset-s5'><button class='btn btn-large waves-effect waves-light start_button' type='submit' name='action'>View Me!<i class='material-icons right'>search</i></button></div></div>";
+        $(".v-center").html(startButton);
+    }
+    startScreen();
+
+    $("main").on("click", ".start_button", function(event){
+        event.preventDefault();
+        window.open('about.html','_self',false);
+    });
 });
