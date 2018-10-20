da869e85138e9ee642889d21a213cb3519781183
JecoLantern <jbayonke@hotmail.com>
2018-10-20 11:54:28 -0700

    fix start button, media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
 
     function startScreen() {
-        startButton = "<div class='row'><div class='col offset-s4'><button class='btn btn-large waves-effect waves-light start_button' type='submit' name='action'>View Me!<i class='material-icons right'>search</i></button></div></div>";
+        startButton = "<div class='row'><div class='col offset-s1'><button class='btn btn-large waves-effect waves-light start_button' type='submit' name='action'>View Me!<i class=' large material-icons right'>search</i></button></div></div>";
         $(".v-center").html(startButton);
     }
     startScreen();
